7d48326d295608c8fd7b3b5dc86467e1d6e328a2
CodeRevo <devrimulasyurtsever00@gmail.com>
2026-01-15 17:04:04 +0300

    file organization

---
+++ items/Lib/atlas.lua
@@ -0,0 +1,124 @@
+SMODS.Atlas({
+	key = "modicon",
+	path = "jud_icon.png",
+	px = 34,
+	py = 34,
+})
+
+SMODS.Atlas({
+	key = "exotics",
+	path = "exotics.png",
+	px = 71,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "enh",
+	path = "enhancements.png",
+	px = 71,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "runes",
+	path = "runes.png",
+	px = 65,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "unoc",
+	path = "unocards.png",
+	px = 71,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "leg",
+	path = "legendary.png",
+	px = 71,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "jepic",
+	path = "epics.png",
+	px = 71,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "uncj",
+	path = "uncommons.png",
+	px = 71,
+	py = 95,
+})
+SMODS.Atlas({
+	key = "rares",
+	path = "rares.png",
+	px = 71,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "cursj",
+	path = "cursed.png",
+	px = 71,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "commons",
+	path = "commons.png",
+	px = 71,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "jplanet",
+	path = "planets.png",
+	px = 71,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "boons",
+	path = "boons.png",
+	px = 42,
+	py = 42,
+})
+
+SMODS.Atlas({
+	key = "avenues",
+	path = "deeds.png",
+	px = 75,
+	py = 99,
+})
+
+SMODS.Atlas({
+	key = "stickers",
+	path = "stickers.png",
+	px = 75,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "poststamps",
+	path = "poststamps.png",
+	px = 75,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "cryptic",
+	path = "cryptics.png",
+	px = 65,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "boosters",
+	path = "boosters.png",
+	px = 71,
+	py = 95,
+})
